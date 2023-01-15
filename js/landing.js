d71e219b09be890c50dd13a83b4f95baed0f4115
João Daniel <vicariuscollectiveofficial@gmail.com>
2023-01-15 16:21:55 +0000

    landing

---
+++ js/landing.js
@@ -0,0 +1,16 @@
+const stars = document.querySelector("#stars");
+const music = document.querySelector("#music");
+
+for (let i = 0; i < 250; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+  star.style.top = `${Math.random() * 100}%`;
+  star.style.left = `${Math.random() * 100}%`;
+  stars.appendChild(star);
+}
+
+setInterval(() => {
+  stars.childNodes.forEach((star) => {
+    star.style.opacity = Math.random();
+  });
+}, 100);
