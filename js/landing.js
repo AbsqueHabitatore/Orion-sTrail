87d94234d264b22fe5be876b22fd41665d30a382
João Daniel <vicariuscollectiveofficial@gmail.com>
2023-01-15 16:29:55 +0000

    2

--- js/landing.js
+++ js/landing.js
@@ -1,16 +1,18 @@
-const stars = document.querySelector("#stars");
-const music = document.querySelector("#music");
+window.addEventListener("load", () => {
+  const stars = document.querySelector("#stars");
+  const music = document.querySelector("#music");
 
-for (let i = 0; i < 250; i++) {
-  const star = document.createElement("div");
-  star.classList.add("star");
-  star.style.top = `${Math.random() * 100}%`;
-  star.style.left = `${Math.random() * 100}%`;
-  stars.appendChild(star);
-}
+  for (let i = 0; i < 250; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    stars.appendChild(star);
+  }
 
-setInterval(() => {
-  stars.childNodes.forEach((star) => {
-    star.style.opacity = Math.random();
-  });
-}, 100);
+  setInterval(() => {
+    stars.children.forEach((star) => {
+      star.style.opacity = Math.random();
+    });
+  }, 100);
+});
